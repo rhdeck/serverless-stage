3fb53b41d43d6afcf7b15e3b2cd22a8f7392a697
Ray Deck <ray@raydeck.com>
2019-05-25 13:18:18 -0400

    Adding profile support to cli

--- cli.js
+++ cli.js
@@ -1,25 +1,36 @@
 #!/usr/bin/env node
 const cp = require("child_process");
-const { findStage } = require("./index");
+const { findStage, findProfile } = require("./index");
 const stage = findStage();
+const profile = findProfile();
 let args = process.argv.slice(2);
+const stageArray = stage ? ["--stage", stage] : [];
+const profileArray = profile ? ["--aws-profile", profile] : [];
 if (args[0] == "setup") {
-  cp.spawnSync("yarn", ["serverless-setup", "--stage", stage], {
-    stdio: "inherit"
-  });
+  cp.spawnSync(
+    "yarn",
+    ["run", "serverless-setup", ...stageArray, ...profileArray],
+    {
+      stdio: "inherit"
+    }
+  );
 } else {
   const setupRequiredCommands = ["deploy"];
   const commands = args.filter(arg => !arg.includes("-"));
 
   if (commands.filter(cmd => setupRequiredCommands.includes(cmd)).length) {
-    cp.spawnSync("yarn", ["serverless-setup", "--stage", stage], {
-      stdio: "inherit"
-    });
+    cp.spawnSync(
+      "yarn",
+      ["run", "serverless-setup", ...stageArray, ...profileArray],
+      {
+        stdio: "inherit"
+      }
+    );
   }
   if (!args.find(arg => arg == "--stage" || arg == "-s")) {
-    args = [...args, "--stage", stage];
+    args = [...args, ...stageArray];
   }
-  cp.spawnSync("yarn", ["sls", ...args], {
+  cp.spawnSync("yarn", ["run", "serverless", ...args, ...profileArray], {
     stdio: "inherit"
   });
 }
