5e5e77254cd7dbab14404d694857b2662e976282
Ray Deck <ray@raydeck.com>
2019-05-25 20:37:11 -0400

    Adding findRegion and getRegion to make more useful out of the gate.

--- index.js
+++ index.js
@@ -54,8 +54,40 @@ function findProfile(dir = process.cwd()) {
   if (parentProfile) _profile = parentProfile;
   return parentProfile;
 }
-
+function findRegion(dir = process.cwd()) {
+  if (_profile) return _profile;
+  //assuming at least one servless dependency in tree has stage defined
+  const { profile, serverless: { dependencies } = {} } = require(path.join(
+    dir,
+    "package.json"
+  ));
+  if (region) {
+    _region = region;
+    return region;
+  }
+  if (!dependencies) return false;
+  const deps = Object.keys(dependencies);
+  let parentProfile = false;
+  const cwd = process.cwd();
+  for (let i = 0; i < deps.length; i++) {
+    let key = deps[i];
+    let nextDir = path.resolve(dependencies[key]);
+    process.chdir(nextDir);
+    parentProfile = findRegion(nextDir);
+    process.chdir(cwd);
+    if (parentProfile) break;
+  }
+  if (parentProfile) _region = parentProfile;
+  return parentProfile;
+}
+function getRegion() {
+  const region = findRegion();
+  if (region) return region;
+  else return "us-east-1"; // environmental default
+}
 module.exports = {
   findStage,
-  findProfile
+  findProfile,
+  findRegion,
+  getRegion
 };
